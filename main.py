02154e2a20abe2fe2ba9ac5fff7d45b0dbb67306
Long Nguyen <longewinn@gmail.com>
2024-02-21 13:03:49 -0600

    implemented test for invalid username and invalid password

--- main.py
+++ main.py
@@ -6,9 +6,42 @@ import time
 # email and password are stored locally in a separate file (variables.py) ignored by git
 from variables import email, password
 
+def test_invalid_user_invalid_pass_login_button():
+  # tests for valid username and valid password using login button
+  driver = webdriver.Firefox()
+
+  #initial navigation to login page
+  navigate_to_login(driver)
+
+  # wait for elements on login to render
+  render_clear_inputs(driver)
+
+  # inputs email into text box
+  email_input = driver.find_element(By.ID, "email")
+  email_input.send_keys("longewinn@gmail.com")
+
+  # sleep for visibility
+  time.sleep(1)
+
+  # inputs password into text box
+  password_input = driver.find_element(By.ID, "password")
+  password_input.send_keys("password1")
+
+  # sleep for visibility
+  time.sleep(1)
+
+  # clicks the login button
+  login_btn = driver.find_element(By.ID, "logIn")
+  login_btn.click()
+
+  time.sleep(3)
+  driver.quit()
+
 def test_valid_user_valid_pass_login_button():
   # tests for valid username and valid password using login button
   driver = webdriver.Firefox()
+
+  #initial navigation to login page
   navigate_to_login(driver)
 
   # wait for elements on login to render
@@ -19,13 +52,15 @@ def test_valid_user_valid_pass_login_button():
   email_input.send_keys(email)
 
   # sleep for visibility
-  time.sleep(1.5)
+  time.sleep(1)
+
   # inputs password into text box
   password_input = driver.find_element(By.ID, "password")
   password_input.send_keys(password)
 
   # sleep for visibility
-  time.sleep(1.5)
+  time.sleep(1)
+
   # clicks the login button
   login_btn = driver.find_element(By.ID, "logIn")
   login_btn.click()
@@ -57,7 +92,7 @@ def navigate_to_login(driver):
     EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/header/div/div[2]"))
   )
   # sleep for visibility
-  time.sleep(1)
+  time.sleep(0.5)
   # selects the dropdown
   login_dropdown = driver.find_element(By.XPATH, "/html/body/div[1]/header/div/div[2]")
   login_dropdown.click()
@@ -66,13 +101,14 @@ def navigate_to_login(driver):
     EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/header/div/div[2]/div/div/div/div/a[1]"))
   )
   # sleep for visibility
-  time.sleep(1)
+  time.sleep(0.5)
   # selects the hudl login to navigate to login page
   hudl_login = driver.find_element(By.XPATH, "/html/body/div[1]/header/div/div[2]/div/div/div/div/a[1]")
   hudl_login.click()
 
 def main():
   test_valid_user_valid_pass_login_button()
+  test_invalid_user_invalid_pass_login_button()
 
 if __name__ == "__main__":
     main()
